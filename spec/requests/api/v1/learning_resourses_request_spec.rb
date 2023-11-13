0130a5f95d2b4a670c44b6dbcb211cc034391e2c
Jorja Fleming <aset284@gmail.com>
2023-11-13 11:24:13 -0700

    Add sad path, refactor

--- spec/requests/api/v1/learning_resourses_request_spec.rb
+++ spec/requests/api/v1/learning_resourses_request_spec.rb
@@ -1,9 +1,9 @@
 
-# require 'rails_helper'
+# require "rails_helper"
 
-# RSpec.describe 'Learning Resources API', type: :request do
-#   describe 'GET /api/v1/learning_resources' do
-#     let(:country_name) { 'Thailand' }
+# RSpec.describe "Learning Resources API", type: :request do
+#   describe "GET /api/v1/learning_resources" do
+#     let(:country_name) { "Thailand" }
 
 #     before do
       
@@ -11,20 +11,20 @@
 #         stub_request(:get, /www.googleapis.com\/youtube\/v3\/search/)
 #           .to_return(
 #             status: 200,
-#             body: File.read('spec/fixtures/youtube_video_response.json'),
-#             headers: { 'Content-Type' => 'application/json' }
+#             body: File.read("spec/fixtures/youtube_video_response.json"),
+#             headers: { "Content-Type" => "application/json" }
 #           )
 
 #       # Stub image search API request to return a sample image response
 #       image_search_stub = stub_request(:get, /example.com\/image_search/)
 #                             .to_return(
 #                               status: 200,
-#                               body: File.read('spec/fixtures/image_search_response.json'),
-#                               headers: { 'Content-Type' => 'application/json' }
+#                               body: File.read("spec/fixtures/image_search_response.json"),
+#                               headers: { "Content-Type" => "application/json" }
 #                             )
 #     end
 
-#     it 'returns learning resources for a specific country' do
+#     it "returns learning resources for a specific country" do
 #       get "/api/v1/learning_resources?country_name=#{country_name}"
 
 #       expect(response).to have_http_status(:ok)
@@ -32,24 +32,24 @@
 #       json_response = JSON.parse(response.body)
 
 #       # Validate the structure of the response
-#       expect(json_response).to include('data')
-#       expect(json_response['data']).to include(
-#         'id' => nil,
-#         'type' => 'learning_resource',
-#         'attributes' => {
-#           'country_name' => country_name,
-#           'video' => {
-#             'title' => 'Sample Video Title',
-#             'video_id' => 'sample_video_id',
+#       expect(json_response).to include("data")
+#       expect(json_response["data"]).to include(
+#         "id" => nil,
+#         "type" => "learning_resource",
+#         "attributes" => {
+#           "country_name" => country_name,
+#           "video" => {
+#             "title" => "Sample Video Title",
+#             "video_id" => "sample_video_id",
 #           },
-#           'images' => [
+#           "images" => [
 #             {
-#               'alt' => 'Sample Alt Text 1',
-#               'url' => 'https://example.com/image1.jpg',
+#               "alt" => "Sample Alt Text 1",
+#               "url" => "https://example.com/image1.jpg",
 #             },
 #             {
-#               'alt' => 'Sample Alt Text 2',
-#               'url' => 'https://example.com/image2.jpg',
+#               "alt" => "Sample Alt Text 2",
+#               "url" => "https://example.com/image2.jpg",
 #             },
 #             # ... more images
 #           ]
@@ -57,10 +57,10 @@
 #       )
 #     end
 
-#     it 'returns an empty response when no videos or images are found' do
+#     it "returns an empty response when no videos or images are found" do
 #       # Adjust stubs to simulate no videos or images found
-#       youtube_stub.to_return(status: 200, body: '{"items": []}', headers: { 'Content-Type' => 'application/json' })
-#       image_search_stub.to_return(status: 200, body: '{"images": []}', headers: { 'Content-Type' => 'application/json' })
+#       youtube_stub.to_return(status: 200, body: "{"items": []}", headers: { "Content-Type" => "application/json" })
+#       image_search_stub.to_return(status: 200, body: "{"images": []}", headers: { "Content-Type" => "application/json" })
 
 #       get "/api/v1/learning_resources?country_name=#{country_name}"
 
@@ -69,14 +69,14 @@
 #       json_response = JSON.parse(response.body)
 
 #       # Validate the structure of the response with empty objects
-#       expect(json_response).to include('data')
-#       expect(json_response['data']).to include(
-#         'id' => nil,
-#         'type' => 'learning_resource',
-#         'attributes' => {
-#           'country_name' => country_name,
-#           'video' => {},
-#           'images' => []
+#       expect(json_response).to include("data")
+#       expect(json_response["data"]).to include(
+#         "id" => nil,
+#         "type" => "learning_resource",
+#         "attributes" => {
+#           "country_name" => country_name,
+#           "video" => {},
+#           "images" => []
 #         }
 #       )
 #     end
--- spec/requests/api/v1/tourist_sites_request_spec.rb
+++ spec/requests/api/v1/tourist_sites_request_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "TouristSites API", type: :request do
       let(:happy_path_country) { "France" }
 
       before do
-  
         stub_request(:get, "https://restcountries.com/v3.1/name/#{happy_path_country}?fields=capital")
           .to_return(
             status: 200,
@@ -64,6 +63,33 @@ RSpec.describe "TouristSites API", type: :request do
           "User-Agent"=>"Faraday v2.7.11"
             }).
           to_return(status: 200, body: File.read("spec/fixtures/country_capital_no_response.json"), headers: {})
+        
+        stub_request(:get, "https://restcountries.com/v3.1/name/Uruguay?fields=capital").
+          with(
+            headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v2.7.11'
+            }).
+          to_return(status: 200, body: File.read("spec/fixtures/country_capital_uruguay_response.json"), headers: {})
+
+        stub_request(:get, "https://restcountries.com/v3.1/capital/Montevideo?fields=latlng").
+          with(
+            headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v2.7.11'
+            }).
+          to_return(status: 200, body: File.read("spec/fixtures/uruguay_latlng_response.json"), headers: {})
+
+        stub_request(:get, "https://api.geoapify.com/v2/places?apiKey=c5962bf636cc4f41a69ab554ae39eb4b&categories=tourism&filter=circle:-56.0,-33.0,10000").
+          with(
+            headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v2.7.11'
+            }).
+          to_return(status: 200, body: File.read("spec/fixtures/tourist_sites_no_response.json"), headers: {})
       end
 
       it "returns an error for an invalid country" do
@@ -75,6 +101,13 @@ RSpec.describe "TouristSites API", type: :request do
 
         expect(json_response).to include("error")
         expect(json_response["error"]).to eq("Capital of #{sad_path_country} not found")
+      end
+
+      it "returns no values for a desolate waste land" do
+        get "/api/v1/tourist_sites?country=Uruguay"
+
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to eq("{\"data\":[]}")
       end
     end
   end
