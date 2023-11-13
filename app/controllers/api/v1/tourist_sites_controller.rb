10c068295131c0dca5275c67c72a03e8018433c5
Jorja Fleming <aset284@gmail.com>
2023-11-13 11:08:13 -0700

    still working

--- app/controllers/api/v1/tourist_sites_controller.rb
+++ app/controllers/api/v1/tourist_sites_controller.rb
@@ -2,9 +2,13 @@ class Api::V1::TouristSitesController < ApplicationController
   def index
     country = params[:country]
     capital = CountriesFacade.capital(country)
-    location = CountriesFacade.latlng(capital)
-    tourist_sites = TouristSitesFacade.destinations(location[1], location[0])
+    if capital.nil?
+      render json: { error: "Capital of #{country} not found" }, status: :not_found
+    else
+      location = CountriesFacade.latlng(capital)
+      tourist_sites = TouristSitesFacade.destinations(location[1], location[0])
 
-    render json: TouristSiteSerializer.new(tourist_sites) 
+      render json: TouristSiteSerializer.new(tourist_sites) 
+    end 
   end
 end
