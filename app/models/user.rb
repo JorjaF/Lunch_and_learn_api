fecdc29ac9a7277791fcc91abe620421651e9f88
Jorja Fleming <aset284@gmail.com>
2023-11-14 12:59:23 -0700

    completed story 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :favorites, dependent: :destroy
   validates_presence_of :name, :password_digest, :email 
   validates_uniqueness_of :email, case_sensitive: false
   before_create :generate_api_key
